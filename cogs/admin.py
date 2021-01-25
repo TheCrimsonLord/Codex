464233c9db45fcff71e660dbb8808012bedeb5d3
Jonathan Fitz <bossminecraftgamer101@gmail.com>
2021-01-25 18:45:27 -0500

    remove old shit

--- cogs/admin.py
+++ cogs/admin.py
@@ -7,24 +7,24 @@ class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(pass_context=True)
+    @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def clear(self, ctx, amount=5):
         await ctx.channel.purge(limit=amount + 1)
 
-    @commands.command(pass_context=True)
+    @commands.command()
     @commands.has_permissions(kick_members=True)
     async def kick(self, ctx, member: discord.Member, *, reason=None):
-        await member.kick(reason=reason) @ commands.command(pass_context=True)
+        await member.kick(reason=reason) @ commands.command()
         await ctx.send(f'Banned {member : discord.Member} for {reason}')
 
-    @commands.command(pass_context=True)
+    @commands.command()
     @commands.has_permissions(ban_members=True)
     async def ban(self, ctx, member: discord.Member, *, reason=None):
         await member.ban(reason=reason)
         await ctx.send(f'Banned {member : discord.Member} for {reason}')
 
-    @commands.command(pass_context=True)
+    @commands.command()
     @commands.has_permissions(ban_members=True)
     async def unban(self, ctx, *, member):
         banned_users = await ctx.guild.bans()
