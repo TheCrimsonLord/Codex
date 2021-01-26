c22d67ccde31b631cbd4b353d372eaf3ac4aa556
Jonathan Fitz <bossminecraftgamer101@gmail.com>
2021-01-25 19:49:06 -0500

    fixed stuff

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,3 +1,5 @@
+import typing
+
 import discord
 from discord.ext import commands
 
@@ -9,33 +11,35 @@ class Admin(commands.Cog):
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
+    @commands.bot_has_permissions(manage_messages=True)
     async def clear(self, ctx, amount=5):
         await ctx.channel.purge(limit=amount + 1)
 
     @commands.command()
     @commands.has_permissions(kick_members=True)
+    @commands.bot_has_permissions(kick_members=True)
     async def kick(self, ctx, member: discord.Member, *, reason=None):
-        await member.kick(reason=reason) @ commands.command()
-        await ctx.send(f'Banned {member : discord.Member} for {reason}')
+        await member.kick(reason=f"{ctx.author} | {reason}")
+        await ctx.send(f'Banned {member} for {reason}')
 
     @commands.command()
     @commands.has_permissions(ban_members=True)
+    @commands.bot_has_permissions(ban_members=True)
     async def ban(self, ctx, member: discord.Member, *, reason=None):
-        await member.ban(reason=reason)
-        await ctx.send(f'Banned {member : discord.Member} for {reason}')
+        await member.ban(reason=f"{ctx.author} | {reason}")
+        await ctx.send(f'Banned {member} for {reason}')
 
     @commands.command()
     @commands.has_permissions(ban_members=True)
-    async def unban(self, ctx, *, member):
+    @commands.bot_has_permissions(ban_members=True)
+    async def unban(self, ctx, *, member: typing.Union[int, str]):
         banned_users = await ctx.guild.bans()
-        member_name, member_discriminator = member.split('#')
 
         for ban_entry in banned_users:
-            user = ban_entry.user
-
-            if (user.name, user.discriminator) == (member_name, member_discriminator):
-                await ctx.guild.unban(user)
-                await ctx.send(f'Unbanned {user.name}#{user.discriminator}')
+            if (isinstance(member, int) and ban_entry.user.id == member) or \
+                    (isinstance(member, str) and member in str(ban_entry.user)):
+                await ctx.guild.unban(ban_entry.user, reason=str(ctx.author))
+                return await ctx.send(f"Unbanned {ban_entry.user}")
 
 
 def setup(bot):
