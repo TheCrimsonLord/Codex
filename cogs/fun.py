8de2611542fdce2d187120d5d0ff72a9cf417f05
TheCrimsonLord <bossminecraftgamer101@gmail.com>
2021-01-27 01:08:45 -0500

    added meme command

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,5 +1,6 @@
 import random
 
+import aiohttp
 import discord
 from discord.ext import commands
 
@@ -37,6 +38,16 @@ class Fun(commands.Cog):
                      "Very doubtful."]
         await ctx.send(f"{question}? {random.choice(responses)}",
                        allowed_mentions=discord.AllowedMentions.none())
+
+    @commands.command(aliases=['memes'])
+    async def meme(self, ctx):
+        embed = discord.Embed(title="Meme", description=None)
+
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get('https://www.reddit.com/r/memes/new.json?sort=hot') as r:
+                res = await r.json()
+                embed.set_image(url=res['data']['children'][random.randint(0, 25)]['data']['url'])
+                await ctx.send(embed=embed, content=None)
 
 
 def setup(bot):
